f85d73feac78ea5259ee38c6074156fcf68719a3
HoaTran1002 <vanhoa0908751520@gmail.com>
2023-05-31 10:05:28 +0700

    fix bug page blog manage

--- src/page/BlogManage/index.tsx
+++ src/page/BlogManage/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import LayoutAdmin from '~/components/layout/LayoutAdmin'
 import { Typography, Grid, Button, Stack } from '@mui/material'
 import image from '~/assets/img/competion-1.jpg'
@@ -21,91 +22,89 @@ const Blog = [
 ]
 const Index = (): JSX.Element => {
   return (
-    <>
-      <LayoutAdmin>
-        <Grid container rowSpacing={2} columnSpacing={{ xs: 2, sm: 2, md: 3 }}>
-          <Grid xs={12}>
-            <Stack direction='row' spacing={20} alignItems='center' sx={{ marginTop: '20px' }}>
-              <Typography variant='h4' sx={{ fontWeight: 500, color: '#1976d2' }}>
-                Quản lý trang blog
-              </Typography>
-              <Button href='/BlogCreate' variant='contained' startIcon={<AddIcon />}>
-                Thêm một blog mới
-              </Button>
-            </Stack>
-          </Grid>
-          {Blog.map((row) => (
-            <Grid xs={6} style={{ paddingRight: '20px' }} key={row.id}>
-              <Stack
-                direction={'row'}
+    <LayoutAdmin>
+      <Grid container rowSpacing={2} columnSpacing={{ xs: 2, sm: 2, md: 3 }}>
+        <Grid xs={12}>
+          <Stack direction='row' spacing={20} alignItems='center' sx={{ marginTop: '20px' }}>
+            <Typography variant='h4' sx={{ fontWeight: 500, color: '#1976d2' }}>
+              Quản lý trang blog
+            </Typography>
+            <Button href='/BlogCreate' variant='contained' startIcon={<AddIcon />}>
+              Thêm một blog mới
+            </Button>
+          </Stack>
+        </Grid>
+        {Blog.map((row) => (
+          <Grid xs={6} style={{ paddingRight: '20px' }} key={row.id}>
+            <Stack
+              direction={'row'}
+              style={{
+                border: '1px solid #e7eeff',
+                display: 'inline-flex',
+                marginTop: '10px',
+                borderRadius: '5px',
+                overflow: 'hidden',
+                width: '100%'
+              }}
+            >
+              <div
+                className=''
                 style={{
-                  border: '1px solid #e7eeff',
-                  display: 'inline-flex',
-                  marginTop: '10px',
-                  borderRadius: '5px',
-                  overflow: 'hidden',
+                  maxHeight: '150px',
+                  width: '140px'
+                }}
+              >
+                <img src={image} alt='' style={{ height: '100%', width: '100%' }} />
+              </div>
+              <div
+                className=''
+                style={{
+                  backgroundColor: 'rgba(25, 118, 210,0.1)',
+                  padding: '10px',
                   width: '100%'
                 }}
               >
-                <div
-                  className=''
+                <Typography
                   style={{
-                    maxHeight: '150px',
-                    width: '140px'
+                    color: '#1976d2',
+                    fontWeight: 'bold',
+                    fontSize: '22px'
                   }}
                 >
-                  <img src={image} alt='' style={{ height: '100%', width: '100%' }} />
-                </div>
-                <div
-                  className=''
+                  {row.heading}
+                </Typography>
+                <Typography
                   style={{
-                    backgroundColor: 'rgba(25, 118, 210,0.1)',
-                    padding: '10px',
-                    width: '100%'
+                    color: '#444'
                   }}
                 >
-                  <Typography
-                    style={{
-                      color: '#1976d2',
-                      fontWeight: 'bold',
-                      fontSize: '22px'
-                    }}
-                  >
-                    {row.heading}
-                  </Typography>
-                  <Typography
-                    style={{
-                      color: '#444'
-                    }}
-                  >
-                    {row.title}
-                  </Typography>
-                </div>
-                <Stack
-                  direction={'column'}
-                  alignItems={'center'}
-                  justifyContent={'center'}
-                  gap={1}
+                  {row.title}
+                </Typography>
+              </div>
+              <Stack
+                direction={'column'}
+                alignItems={'center'}
+                justifyContent={'center'}
+                gap={1}
+                style={{
+                  padding: '8px'
+                }}
+              >
+                <Button
+                  href='/BlogDetail'
+                  variant='outlined'
                   style={{
-                    padding: '8px'
+                    width: '120px'
                   }}
                 >
-                  <Button
-                    href='/BlogDetail'
-                    variant='outlined'
-                    style={{
-                      width: '120px'
-                    }}
-                  >
-                    Chỉnh Sủa
-                  </Button>
-                </Stack>
+                  Chỉnh Sủa
+                </Button>
               </Stack>
-            </Grid>
-          ))}
-        </Grid>
-      </LayoutAdmin>
-    </>
+            </Stack>
+          </Grid>
+        ))}
+      </Grid>
+    </LayoutAdmin>
   )
 }
 
