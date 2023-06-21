89f3f1360bfc49897c66ba7bf7bc5e7cbbe63b70
HoaTran1002 <vanhoa0908751520@gmail.com>
2023-06-22 00:20:27 +0700

    add all

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,12 +2,9 @@ import axios from 'axios'
 import base_url from '~/config/env'
 
 const server = axios.create({
-<<<<<<< HEAD
   baseURL: base_url,
-=======
-  baseURL: 'http://103.98.161.6:5050/api',
->>>>>>> refs/remotes/origin/main
-  withCredentials: false
+
+  withCredentials: true
 })
 
 export default server
